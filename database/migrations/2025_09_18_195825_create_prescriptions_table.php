b39d42c7f8d866f8af60772757e5bd869686811b
Yandé Guissé <yandeiguiss2@gmail.com>
2025-09-18 20:25:56 +0000

    models et migrations

---
+++ database/migrations/2025_09_18_195825_create_prescriptions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prescriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('contenu');
+            $table->unsignedBigInteger('medecin_id');
+            $table->unsignedBigInteger('patient_id');
+            $table->unsignedBigInteger('structure_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prescriptions');
+    }
+};
